96028f7f30468598cf3766e7d1212ab9eb821481
Bryan Jazo <bryanjazo88@gmail.com>
2021-05-15 20:17:13 -0700

    started auth for user

--- src/index.js
+++ src/index.js
@@ -21,62 +21,87 @@ back = document.getElementById('dashboard')
 // Log in
 document.getElementById('logInUser').addEventListener('click', logInUser)
 
-function logInUser(){
+function logInUser() {
   console.log('call')
   let email = document.getElementById('email').value
   let password = document.getElementById('password').value
 
-  firebase.auth().signInWithEmailAndPassword(email, password).catch(e=>{
+  firebase.auth().signInWithEmailAndPassword(email, password).catch(e => {
     console.log(e)
   })
 }
-function showUserHome(user){
+
+function showUserHome(user) {
   document.getElementById('userDetails').innerHtml = `
   <P>Logged In Success with ${user.email}`
 }
-firebase.auth().onAuthStateChanged(user=>{
-  if(user){
+firebase.auth().onAuthStateChanged(user => {
+  if (user) {
     console.log(user)
     front.style.display = "none"
     back.style.display = "block"
     showUserHome(user)
-  }else{
+  } else {
     front.style.display = "block"
     back.style.display = "none"
   }
 })
 // Sign Up
+function createUser(email, uid) {
+  let config = {
+    method: "POST",
+    mode: "no-cors",
+    headers:{
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      email: email,
+      uid: uid
+    })
+  }
+  fetch('http://localhost:3000/users', config)
+    .then(res => res.json())
+    .then(obj => console.log(obj))
+}
 
 document.getElementById('btnSignUpO').addEventListener('click', signUpUser)
-function signUpUser(){
-  console.log('call')
+
+function signUpUser() {
   let email = document.getElementById('emaill').value
   let password = document.getElementById('passwordd').value
 
   firebase.auth().createUserWithEmailAndPassword(email, password)
-  .then((userCredential) => {
+    .then((userCredential) => {
+      // Signed in
+      var user = userCredential.user;
+      createUser(firebase.auth().createUser().email, firebase.auth().createUser().uid)
 
-  })
-  .catch(e=>{
-    console.log(e)
-  })
+      // ...
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // ..
+    });
 }
 
-firebase.auth().onAuthStateChanged(user=>{
-  if(user){
+firebase.auth().onAuthStateChanged(user => {
+  if (user) {
     console.log(user)
-  }else{
+  } else {
 
   }
 })
 
 // Log Out
 document.getElementById('logoutBtn').addEventListener('click', logOutUser)
-function logOutUser(){
-  firebase.auth().signOut().then(()=>{
+
+function logOutUser() {
+  firebase.auth().signOut().then(() => {
     front.style.display = "block"
     back.style.display = "none"
-  }).catch(e=>{
+  }).catch(e => {
     console.log(e)
   })
 }
