dbec66be7fad6cffa78c8d69ece1526ce3bc3348
Bryan Jazo <bryanjazo88@gmail.com>
2021-06-04 11:38:13 -0700

    finished re organizer

--- src/components/likes.js
+++ src/components/likes.js
@@ -31,7 +31,25 @@ function organizedMethod([...likesArray]){
     return 0
   })
   console.log(likesArray)
+  return likesArray.map(newLikes).join(' ')
 }
+
+function newLikes(likes){
+  console.log('here')
+  return(  `
+    <div id="appendProfile" class="moveLikes ">
+     <div class="tinderCards__likes swipeTwo ">
+      <div  class"allCards" style='background-image: url(${likes.image});'>
+       <div  class="card">
+            <img id="imageCard"src="${likes.image}" alt="">
+            <h3 id='name' class="nameText">name: ${likes.name}, gender: ${likes.gender}, interest: ${likes.interest}</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+    `)
+}
+
 function likesContainer(){
   const promise = fetch(`http://localhost:3000/users/${sessionStorage.userID}/likes`)
   promise.then(function(resp) {
