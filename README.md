39b0d38c5373022a6f38061dbc3b8c65ab576d43
Bryan Jazo <bryanjazo88@gmail.com>
2022-01-06 13:56:05 -0800

    lk

--- README.md
+++ README.md
@@ -20,3 +20,55 @@ Please make sure to update tests as appropriate.
 
 ## License
 https://github.com/Bryanjazo/TinDog/blob/main/LICENSE
+
+// Sign Up Form 
+
+signUpForm.addEventListener('submit', function(e) {
+  e.preventDefault()
+  fetch('https://tindog-backend-bryan.herokuapp.com/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      body: JSON.stringify({
+        username: inputFields[0].value,
+        email: inputFields[1].value,
+        password: inputFields[2].value
+      })
+    })
+    .then(res => res.json())
+    .then(function(object) {
+      let obj = object
+      localStorage.profile_id = obj.profile_id
+      sessionStorage.userID = obj.id
+      displayUserHome();
+    })
+})
+
+
+// Login Form
+
+logInForm.addEventListener('submit', function(e) {
+  e.preventDefault()
+  console.log('here')
+
+  fetch('https://tindog-backend-bryan.herokuapp.com/sessions', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      body: JSON.stringify({
+        email: logInFields[0].value,
+        password: logInFields[1].value
+      })
+    })
+    .then(res => res.json())
+    .then(function(object) {
+      let obj = object
+      sessionStorage.userID = obj.id
+      localStorage.profile_id = obj.profile_id
+      console.log(object)
+    })
+})
--- src/index.js
+++ src/index.js
@@ -7,12 +7,156 @@ document.addEventListener('DOMContentLoaded', () => {
   tinDogCards()
   displayUserHome()
   swipeButtons()
-  navBard()
+  navBar()
 
 
 })
 
 
+
+
+
+// Urls
+const BASE_URL = "https://tindog-backend-bryan.herokuapp.com"
+const USERS_URL = `${BASE_URL}/users`
+
+var left_cover = document.getElementById("left-cover");
+var left_form = document.getElementById("left-form");
+var right_cover = document.getElementById("right-cover");
+var right_form = document.getElementById("right-form");
+
+// Variables for pushing user to databse
+function switchSignup() {
+  right_form.classList.add("fade-in-element");
+  left_cover.classList.add("fade-in-element");
+  left_form.classList.add("form-hide");
+  left_cover.classList.remove("cover-hide");
+  right_form.classList.remove("form-hide");
+  right_cover.classList.add("cover-hide");
+}
+// hide Appear Log in and Sign up
+front = document.getElementById('signup-signIn')
+back = document.getElementById('dashboard')
+//sign Up
+const signUpForm = document.querySelector(".signup-form")
+const inputFields = document.querySelectorAll(".input-text")
+
+// Log in
+const logInForm = document.querySelector(".login-form")
+const logInFields = document.querySelectorAll(".input-box")
+
+
+
+// Login 
+
+function logInUser() {
+  console.log('call')
+  let email = document.getElementById('email').value
+  let password = document.getElementById('password').value
+
+  firebase.auth().signInWithEmailAndPassword(email, password).catch(e => {
+    console.log(e)
+
+  })
+}
+
+function showUserHome(user) {
+  document.getElementById('userDetails').innerHtml = "logged in"
+}
+firebase.auth().onAuthStateChanged(user => {
+  if (user) {
+    console.log(user)
+    front.style.display = "none"
+    back.style.display = "block"
+    showUserHome(user)
+  } else {
+    front.style.display = "block"
+    back.style.display = "none"
+  }
+})
+
+
+// sign Up 
+
+function signUpUser() {
+  console.log('call')
+  let email = document.getElementById('emaill').value
+  let password = document.getElementById('passwordd').value
+
+  firebase.auth().createUserWithEmailAndPassword(email, password).then(auth => {
+      firebase.storageBucket().ref('users.' + auth.user.uid + '/profile.jpg').put(file).then(function() {
+        console.log('succesfully uploaded')
+      })
+    })
+    .then((userCredential) => {
+      // Signed in
+      var user = userCredential.user;
+
+
+      // ...
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // ..
+    });
+}
+
+// connecting to firebase for user oauth
+
+firebase.auth().onAuthStateChanged(user => {
+  if (user) {
+    console.log(user)
+  } else {
+
+  }
+})
+// adding profile pic
+imgSrcDisplay = document.getElementById('img')
+let file = {};
+function chooseFile(e){
+ file = e.target.files[0];
+}
+
+function signUpButtonPressed(){
+  let email = document.getElementById('emaill').value
+  let password = document.getElementById('passwordd').value
+  firebase.auth().createUserWithEmailAndPassword(email, password).then(auth => {
+
+    firebase.storage().ref('users/' + auth.user.uid + '/profile.jpg').put(file).then(function () {
+      console.log('success')
+    })
+    console.log(auth);
+  }).catch(e => {
+    console.log(e)
+  })
+}
+
+firebase.auth().onAuthStateChanged(user => {
+  if(user){
+    firebase.storage().ref('users/' + user.uid + '/profile.jpg').getDownloadURL().then(imgUrl => {
+      console.log(imgSrcDisplay.src = imgUrl)
+      imgSrcDisplay.src = imgUrl
+
+    })
+  }
+})
+
+// Log Out
+
+
+function logOutUser() {
+  firebase.auth().signOut().then(() => {
+    sessionStorage.userID = ""
+    front.style.display = "block"
+    back.style.display = "none"
+
+  }).catch(e => {
+    console.log(e)
+  })
+}
+
+// ----------------------------------- Home -------------------------------
 // Event listeners
 document.getElementById('logInUser').addEventListener('click', logInUser)
 document.getElementById('btnSignUpO').addEventListener('click', signUpUser)
@@ -20,7 +164,7 @@ let buttonSwipe = swipeButtons();
 
 
 
-function navBard() {
+function navBar() {
   let headerDiv = document.getElementById('headerID')
   document.getElementById('logoutBtn').addEventListener('click', logOutUser)
   headerDiv.addEventListener('click', function(e) {
@@ -151,194 +295,5 @@ function homeIcons() {
         alert("no more matches")
       }
     }
-  })
-}
-
-
-
-// Urls
-const BASE_URL = "https://tindog-backend-bryan.herokuapp.com"
-const USERS_URL = `${BASE_URL}/users`
-
-var left_cover = document.getElementById("left-cover");
-var left_form = document.getElementById("left-form");
-var right_cover = document.getElementById("right-cover");
-var right_form = document.getElementById("right-form");
-
-// Variables for pushing user to databse
-function switchSignup() {
-  right_form.classList.add("fade-in-element");
-  left_cover.classList.add("fade-in-element");
-  left_form.classList.add("form-hide");
-  left_cover.classList.remove("cover-hide");
-  right_form.classList.remove("form-hide");
-  right_cover.classList.add("cover-hide");
-}
-// hide Appear Log in and Sign up
-front = document.getElementById('signup-signIn')
-back = document.getElementById('dashboard')
-//sign Up
-const signUpForm = document.querySelector(".signup-form")
-const inputFields = document.querySelectorAll(".input-text")
-
-// Log in
-const logInForm = document.querySelector(".login-form")
-const logInFields = document.querySelectorAll(".input-box")
-
-// Login Form
-
-logInForm.addEventListener('submit', function(e) {
-  e.preventDefault()
-  console.log('here')
-
-  fetch('https://tindog-backend-bryan.herokuapp.com/sessions', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-      },
-      body: JSON.stringify({
-        email: logInFields[0].value,
-        password: logInFields[1].value
-      })
-    })
-    .then(res => res.json())
-    .then(function(object) {
-      let obj = object
-      sessionStorage.userID = obj.id
-      localStorage.profile_id = obj.profile_id
-      console.log(object)
-    })
-})
-
-// Login 
-
-function logInUser() {
-  console.log('call')
-  let email = document.getElementById('email').value
-  let password = document.getElementById('password').value
-
-  firebase.auth().signInWithEmailAndPassword(email, password).catch(e => {
-    console.log(e)
-
-  })
-}
-
-function showUserHome(user) {
-  document.getElementById('userDetails').innerHtml = "logged in"
-}
-firebase.auth().onAuthStateChanged(user => {
-  if (user) {
-    console.log(user)
-    front.style.display = "none"
-    back.style.display = "block"
-    showUserHome(user)
-  } else {
-    front.style.display = "block"
-    back.style.display = "none"
-  }
-})
-// Sign Up Form 
-
-signUpForm.addEventListener('submit', function(e) {
-  e.preventDefault()
-  fetch('https://tindog-backend-bryan.herokuapp.com/users', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-      },
-      body: JSON.stringify({
-        username: inputFields[0].value,
-        email: inputFields[1].value,
-        password: inputFields[2].value
-      })
-    })
-    .then(res => res.json())
-    .then(function(object) {
-      let obj = object
-      localStorage.profile_id = obj.profile_id
-      sessionStorage.userID = obj.id
-      displayUserHome();
-    })
-})
-
-// sign Up 
-
-function signUpUser() {
-  console.log('call')
-  let email = document.getElementById('emaill').value
-  let password = document.getElementById('passwordd').value
-
-  firebase.auth().createUserWithEmailAndPassword(email, password).then(auth => {
-      firebase.storageBucket().ref('users.' + auth.user.uid + '/profile.jpg').put(file).then(function() {
-        console.log('succesfully uploaded')
-      })
-    })
-    .then((userCredential) => {
-      // Signed in
-      var user = userCredential.user;
-
-
-      // ...
-    })
-    .catch((error) => {
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      // ..
-    });
-}
-
-// connecting to firebase for user oauth
-
-firebase.auth().onAuthStateChanged(user => {
-  if (user) {
-    console.log(user)
-  } else {
-
-  }
-})
-// adding profile pic
-imgSrcDisplay = document.getElementById('img')
-let file = {};
-function chooseFile(e){
- file = e.target.files[0];
-}
-
-function signUpButtonPressed(){
-  let email = document.getElementById('emaill').value
-  let password = document.getElementById('passwordd').value
-  firebase.auth().createUserWithEmailAndPassword(email, password).then(auth => {
-
-    firebase.storage().ref('users/' + auth.user.uid + '/profile.jpg').put(file).then(function () {
-      console.log('success')
-    })
-    console.log(auth);
-  }).catch(e => {
-    console.log(e)
-  })
-}
-
-firebase.auth().onAuthStateChanged(user => {
-  if(user){
-    firebase.storage().ref('users/' + user.uid + '/profile.jpg').getDownloadURL().then(imgUrl => {
-      console.log(imgSrcDisplay.src = imgUrl)
-      imgSrcDisplay.src = imgUrl
-
-    })
-  }
-})
-
-// Log Out
-
-
-function logOutUser() {
-  firebase.auth().signOut().then(() => {
-    sessionStorage.userID = ""
-    front.style.display = "block"
-    back.style.display = "none"
-
-  }).catch(e => {
-    console.log(e)
   })
 }
